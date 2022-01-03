b802145a6bcad1ed12eaec360e60cd68daf29d77
tomasmisutka <tomasmisutka@gmail.com>
2022-01-03 14:50:53 +0100

    Initial Commit

---
+++ Shared/WarrantyReminderSwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  WarrantyReminderSwiftUIApp.swift
+//  Shared
+//
+//  Created by Tomáš Mišutka on 03/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WarrantyReminderSwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
