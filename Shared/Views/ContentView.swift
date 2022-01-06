49edd207f3e1e51f100caa2970551b55ab011d5f
tomasmisutka <tomasmisutka@gmail.com>
2022-01-06 14:29:51 +0100

    no notification permission alert, request for notification permission in first start of app

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import CoreData
+import UserNotifications
 
 struct ContentView: View
 {
@@ -17,7 +18,7 @@ struct ContentView: View
         animation: .default)
     
     private var products: FetchedResults<Product>
-    @State private var refresh: Bool = false
+    @State private var usingNotification: Bool = false
 
     var body: some View
     {
@@ -46,6 +47,41 @@ struct ContentView: View
                 ToolbarItem(placement: .navigationBarLeading)
                 { AppIcon() }
             }
+        }
+        .onAppear()
+        {
+            let current = UNUserNotificationCenter.current()
+            current.getNotificationSettings(completionHandler:
+            { permission in
+                switch permission.authorizationStatus
+                {
+                    case .authorized:
+                        print("User granted permission for notification")
+                        usingNotification = false
+                    case .denied:
+                        print("User denied notification permission")
+                        usingNotification = true
+                    case .notDetermined:
+                        print("Notification permission haven't been asked yet")
+                        usingNotification = false
+                        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                        if success { print("All set!") }
+                        else if let error = error { print(error.localizedDescription) }
+                    }
+                    case .provisional:
+                        print("The application is authorized to post non-interruptive user notifications.")
+                        usingNotification = true
+                    case .ephemeral:
+                        print("The application is temporarily authorized to post notifications. Only available to app clips.")
+                    @unknown default:
+                        print("Unknow Status")
+                        usingNotification = false
+                }
+            })
+        }
+        .alert("Notification are disabled. To Correct APP functionality, please enable notification in Settings", isPresented: $usingNotification)
+        {
+            Button("OK", role: .cancel) { }
         }
     }
 
